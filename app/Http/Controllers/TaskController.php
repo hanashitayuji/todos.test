60499cdd253f56a8b3862ade9165cafce82138d2
hanashitayuji <baske_0119@icloud.com>
2023-02-11 15:37:45 +0900

    タスク一覧表示

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,16 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Database\Seeder;
 use App\Models\Folder;
+use App\Models\Task; //追加
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id) {
-        $folders = Folder::all();
+    // すべてのフォルダを取得する
+    $folders = Folder::all();
 
-        return view('tasks/index', [
-            'folders' => $folders,
-            'current_folder_id' => $id,
-        ]);
+    // 選ばれたフォルダを取得する
+    $current_folder = Folder::find($id); 
+
+    // 選ばれたフォルダに紐づくタスクを取得する
+    $tasks = $current_folder->tasks()->get();
+
+    return view('tasks/index', [
+        'folders' => $folders,
+        'current_folder_id' => $current_folder->id,
+        'tasks' => $tasks,
+    ]);
     }
 }
